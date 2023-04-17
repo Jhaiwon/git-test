1fe3bf23024040551824b0177d55564e8f45eb78
haiwon.Jeong <hainni120213@gmail.com>
2023-04-17 09:42:06 +0900

    mm

--- Random.c
+++ Random.c
@@ -1,145 +1,28 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-int main()
-{
-	int i;
-	char name32[33][8]={"한가인", "김태희", "최지우", "김아중", "윤아", "티파니", "나르샤", "가인", "한채영", "송혜교", 
- "선미", "소희", "박정아", "서인영", "고아라", "이연희", "황정음", "신세경", "한지민", "문근영","신봉선", "김신영",  
-"전지현", "이나영", "박신혜", "윤은혜", "정려원", "박은혜", "한효주", "박민영", "김민정", "고은아"};
-	char fav_name[7];
-	char name16[16][7];
-	char name8[8][7];
-	char name4[4][7];
-	char name_f[2][10];
-	char winner[1][7];
-	
-	printf("\t\t지 금 시 작 합 니 다\n");
-	printf("\n------------------------32강------------------------\n");
-	sleep(600);
-	for(i=0; i<32; i+=2)
-	{
-		fflush(stdin);
-		printf("\n%s %s\n",name32[i],name32[i+1]);
-		printf("선택!!: ");scanf("%s",fav_name);
-		if(strcmp(fav_name,name32[i])==0)
-		{
-			strcpy(name16[i/2],fav_name);
-		
-		}
-		else if(strcmp(fav_name,name32[i+1])==0)
-		{
-			strcpy(name16[i/2],fav_name);
-		
-		}
-		else
-		{
-			printf("세글자도 못치는 바보.. 흑... 커...컨..트...로..ㄹ..씨....좀...눌ㄹ...ㅓ\n");
-			return 0;
-		}
-		
-	}
-	fflush(stdin);
+#include <pthread.h>
 
-	printf("------------------------16강------------------------\n");
-	printf("\n시작됩니다.........\n");
-	Sleep(400);
+void *thread_func(void *arg) {
+    int i;
+    int *tsum = (int *)arg;
+    int total = 0;
 
-	for(i=0; i<16; i+=2)
-	{
-		fflush(stdin);
-		printf("\n%s %s\n",name16[i],name16[i+1]);
-		printf("선택!!: ");scanf("%s",fav_name);
-		if(strcmp(fav_name,name16[i])==0)
-		{
-			strcpy(name8[i/2],fav_name);
-			
-		}
-		else if(strcmp(fav_name,name16[i+1])==0)
-		{
-			strcpy(name8[i/2],fav_name);
-		
-		}
-		else
-		{
-			printf("세글자도 못치는 바보.. 흑...커...컨..트...로..ㄹ..씨....좀...눌ㄹ...ㅓ\n");
-			return 0;
-		}
-		
-	}
-	fflush(stdin);
-	
-	printf("------------------------8강------------------------\n");
-	printf("\n시작됩니다.........\n");
-	Sleep(400);
+    for (i = 0; i < 100; i++) {
+        total += i;
+    }
 
-	for(i=0; i<8; i+=2)
-	{
-		fflush(stdin);
-		printf("\n%s %s\n",name8[i],name8[i+1]);
-		printf("선택!!: ");scanf("%s",fav_name);
-		if(strcmp(fav_name,name8[i])==0)
-		{
-			strcpy(name4[i/2],fav_name);
-		
-		}
-		else if(strcmp(fav_name,name8[i+1])==0)
-		{
-			strcpy(name4[i/2],fav_name);
-			
-		}
-		else
-		{
-			printf("세글자도 못치는 바보.. 흑...커...컨..트...로..ㄹ..씨....좀...눌ㄹ...ㅓ\n");
-			return 0;
-		}
-		
-	}
-	fflush(stdin);
+    *tsum = total;
 
-	printf("------------------------4강------------------------\n");
-	printf("\n시작됩니다.........\n");
-	Sleep(400);
+    printf("tsum = %d\n", *tsum);
+}
 
-	for(i=0; i<4; i+=2)
-	{
-		fflush(stdin);
-		printf("\n%s %s\n",name4[i],name4[i+1]);
-		printf("선택!!: ");scanf("%s",fav_name);
-		if(strcmp(fav_name,name4[i])==0)
-		{
-			strcpy(name_f[i/2],fav_name);
-			
-		}
-		else if(strcmp(fav_name,name4[i+1])==0)
-		{
-			strcpy(name_f[i/2],fav_name);
-			
-		}
-		else
-		{
-			printf("세글자도 못치는 바보.. 흑...커...컨..트...로..ㄹ..씨....좀...눌ㄹ...ㅓ.\n");
-			return 0;
-		}
-		
-	}
+int main() {
+    pthread_t tid;
+    int tsum;
 
-	printf("★★★★★★★★★결승전★★★★★★★★★\n");
-	
-	Sleep(2000);
+    pthread_create(&tid, NULL, thread_func, &tsum);
+    pthread_join(tid, NULL);
 
-	fflush(stdin);
-	printf("\n%s %s\n",name_f[0],name_f[1]);
-	printf("선택!!: ");scanf("%s",fav_name);
-	strcpy(winner[1],fav_name);
-	if(strcmp(fav_name,name_f[0])==0){}
-	else if(strcmp(fav_name,name_f[1])==0){}
-	
-	Sleep(3000);
-	printf("\n*****************최종 우승은 %s!!!!*****************\n",winner[1]);
+    printf("total = %d\n", tsum);
 
-	
-	printf("\nCtrl+c를 눌러 종료 시키십시요\n");
-	Sleep(10000);
-	return 0;
-	}
+    return 0;
+}
---
+++ test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    int i,j;
+    int Count;
+
+    scanf("%d",&i);
+    j=i--;
+
+    if(j>0,)
+}
